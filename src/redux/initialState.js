78957451645655a67ee1db26cbc67a9ff92991a2
azin-a <aalahkhani@gmail.com>
2022-12-26 07:04:52 +0330

    initchange

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -3,10 +3,8 @@ export const initialState = {
         {name: "negin"}
     ],
     tasks: [
-       { title: "do laundry", name:"nina", id:1},
-       { title: "call dad.", name:"jim",id:2},
-    //    { title: "buy dinner.", name:"ashkan",id:"3"},
-    //    { title: "study.", name:"negin",id:"4"},
-    //    { title: "make tea.", name:"azin",id:"5"},
+       { title: "do laundry", name:"Nina", id:1},
+       { title: "call dad.", name:"Jim",id:2}
     ]
+    
 }
